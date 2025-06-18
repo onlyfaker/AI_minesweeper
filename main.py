3057f522092ffe87327b886c71f5a2213e931c4f
Gemaljevic Aleksandar <67740316+onlyfaker@users.noreply.github.com>
2025-06-17 22:31:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
-# TODO - learning more on AI's and Agents.
-# TODO - trying out n8n agent building
+# 1. RULE - if a tile has the same amount of hidden squares around it as unflaged
+# 2. RULE - if a tile has the same amount of flags around it as the number on the square,
+            # then all remaining hidden tiles around it are not bombs
+# 3. RULE - implement logical rules - probability of each square being a bomb
