bafe65b66c012255030ae7f569c611dfd20d5c7d
Gemaljevic Aleksandar <67740316+onlyfaker@users.noreply.github.com>
2025-06-17 22:48:34 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,4 @@
 # 2. RULE - if a tile has the same amount of flags around it as the number on the square,
             # then all remaining hidden tiles around it are not bombs
 # 3. RULE - implement logical rules - probability of each square being a bomb
+# PROBABILITY FORMULA - P = (bomb arrangements with bomb in target square)/(total possible arrangements)
